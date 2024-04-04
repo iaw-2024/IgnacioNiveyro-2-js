9a5f0422e0c6f0e09e143922e628a289add1d013
IgnacioNiveyro <nacho_niveyro@hotmail.com>
2024-04-04 16:42:53 -0300

    probando vercel

--- public/express/respuestaServidor.js
+++ public/express/respuestaServidor.js
@@ -5,6 +5,7 @@ function respuestaServidor(req, res) {
     // Convertir los datos JSON en una tabla HTML
     let tablaHTML = "<table border='1'>";
     tablaHTML += "<tr><th>Nombre</th><th>Apellido</th></tr>";
+    // Recorrer los datos y construir la tabla
     datos.forEach(dato => {
         tablaHTML += `<tr><td>${dato.nombre}</td><td>${dato.apellido}</td></tr>`;
     });
